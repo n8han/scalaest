74c5673e8ca5ed93f1621967250d08dd52a99913
Nathan Hamblen <nathan@technically.us>
2009-09-12 01:24:10 -0400

    count test

--- src/main/scala/intermediate/judge.scala
+++ src/main/scala/intermediate/judge.scala
@@ -7,5 +7,5 @@ trait Judge {
 object ScalaestJudge extends MockJudge
 
 class MockJudge extends Judge {
-  def judge(url: String) = ScalaestCounter.count("scala scala scala")
+  def judge(url: String) = ScalaestCounter.count("scala scala scala escalada scaloo scala")
 }
