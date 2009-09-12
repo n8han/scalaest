77631a860b9867350846729836043e5530441fb3
Nathan Hamblen <nathan@technically.us>
2009-09-12 00:47:48 -0400

    breaking out admin

--- src/main/scala/intermediate/judge.scala
+++ src/main/scala/intermediate/judge.scala
@@ -4,7 +4,21 @@ trait Judge {
   def judge(url: String): Int
 }
 
-object ScalaestJudge extends MockJudge
+object ScalaestJudge extends SolutionJudge
+
+class SolutionJudge extends Judge {
+  import scala.io.Source
+  def judge(url: String) = {
+    println("Referencing %s..." format url)
+    try {
+      (0 /: Source.fromURL(url).getLines) {
+        _ + ScalaestCounter.count(_)
+      }
+    } catch {
+      case e => println(e.getMessage); 0
+    }
+  }
+}
 
 class MockJudge extends Judge {
   def judge(url: String) = ScalaestCounter.count("scala scala scala")
