5c2e33feea8e51eb3d62a4cf83911fec17826f79
Nathan Hamblen <nathan@technically.us>
2009-09-14 01:43:03 -0400

    dispatch final

--- project/build/ScalaestProject.scala
+++ project/build/ScalaestProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class ScalaestProject(info: ProjectInfo) extends DefaultProject(info) {
-  val dispatch = "net.databinder" %% "dispatch-http" % "0.6.0-SNAPSHOT"
+  val dispatch = "net.databinder" %% "dispatch-http" % "0.6.0"
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 }
 
